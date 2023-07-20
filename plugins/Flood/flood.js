8c7f5ba87f47b20937bf0179831b7cc416e451d4
psyckh <134656091+psyckh@users.noreply.github.com>
2023-07-20 02:46:57 -0300

    Update flood.js

--- plugins/Flood/flood.js
+++ plugins/Flood/flood.js
@@ -1,1 +1,18 @@
+// Definindo o objeto do plugin
+const MeuPlugin = {
+  addNumbers: (num1, num2) => {
+    // Implementação do gancho addNumbers
+    const resultado = num1 + num2;
+    return resultado;
+  },
+};
 
+// Exemplo de uso do plugin
+// Imagine que esse código está dentro do seu sistema ou aplicativo
+
+// Importe ou inclua o arquivo que contém o plugin
+
+// Use o gancho addNumbers
+const resultadoAdicao = MeuPlugin.addNumbers(5, 3);
+console.log("Resultado da Adição:", resultadoAdica
+            o);
